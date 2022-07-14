ceb9ef4442b487ea646f2e2378f5665108e32379
Donik <umarovdoniyor096@gmail.com>
2022-07-15 01:49:25 +0500

    Initial commit

---
+++ day_book/diary/models.py
@@ -0,0 +1,31 @@
+
+from django.db import models
+import os
+# Create your models here.
+from rest_framework.exceptions import ValidationError
+
+class Student(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=False)
+    age = models.IntegerField(blank=True, null=False)
+    grade = models.IntegerField(blank=True, null=False)
+    attendance = models.BooleanField(blank=True, null=False, default=True)
+    homework = models.BooleanField(blank=True, null=False, default=True)
+
+
+class Group(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=False)
+    age = models.IntegerField(blank=True, null=False)
+    subject = models.CharField(max_length=50, blank=True, null=False)
+    group_id = models.OneToOneField(Group, on_delete=models.CASCADE, null=True, blank=True)
+
+
+
+
+
+class Message(models.Model):
+    body = models.TextField(blank=True, null=False)
+    phone = models.IntegerField(blank=True, null=False)
